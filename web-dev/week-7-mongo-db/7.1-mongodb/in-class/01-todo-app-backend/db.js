05136021db9b7f232cc6da8b82bc437b3ef2c809
Keshav Dutt Kaushik <codemxde23@gmail.com>
2025-02-26 01:56:14 +0530

    committing some leftover files

--- web-dev/week-7-mongo-db/7.1-mongodb/in-class/01-todo-app-backend/db.js
+++ web-dev/week-7-mongo-db/7.1-mongodb/in-class/01-todo-app-backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const ObjectId = mongoose.ObjectId;
+const ObjectId = Schema.Types.ObjectId;
 
 const User = new Schema({
   email: { type: String, unique: true },
